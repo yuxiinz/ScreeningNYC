1bdb8ee8e167bca4e76a17d712b0e24fe5d12fcd
yuxiinz <yuxinzhu0209@gmail.com>
2026-04-06 00:43:12 -0400

    fixed map

--- app/(browse)/map/page.tsx
+++ app/(browse)/map/page.tsx
@@ -1,8 +1,11 @@
 // app/(browse)/map/page.tsx
 import MapPageClient from '@/components/map/MapPageClient'
+import { connection } from 'next/server'
 import { getCachedMapTheaters } from '@/lib/cache/public-data'
 
 export default async function MapPage() {
+  await connection()
+
   const theaters = await getCachedMapTheaters()
 
   return (
