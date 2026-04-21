26493f59d2d681faaacb842ec23d94333f1a5bb9
yuxiinz <yuxinzhu0209@gmail.com>
2026-04-20 22:53:49 -0400

    clean up films api

--- app/films/layout.tsx
+++
@@ -1,9 +0,0 @@
-import SiteShell from '@/components/SiteShell'
-
-export default function FilmsLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return <SiteShell>{children}</SiteShell>
-}
